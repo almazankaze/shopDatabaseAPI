b2dadc68bf860b4e097cff228360ead8a5bc0be0
John Almazan <almazankaze@live.com>
2023-11-24 00:12:56 -0600

    added simple security checks

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import passport from "passport";
 import LocalStrategy from "passport-local";
 import session from "express-session";
+import mongoSanitize from "express-mongo-sanitize";
 
 import products from "./routes/products.js";
 import reviews from "./routes/reviews.js";
@@ -20,9 +21,10 @@ dotenv.config();
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use(mongoSanitize());
 
 const sessionConfig = {
-  name: "session",
+  name: "trust-alma",
   secret: process.env.SECRET,
   resave: false,
   saveUninitialized: false,
@@ -49,6 +51,7 @@ passport.deserializeUser(User.deserializeUser());
 app.use((req, res, next) => {
   res.locals.currentUser = req.user;
 
+  console.log(req.params);
   next();
 });
 
--- schemas.js
+++ schemas.js
@@ -1,18 +1,40 @@
-import joi from "joi";
+import BaseJoi from "joi";
+import sanitizeHtml from "sanitize-html";
 
-const Joi = joi;
+const extension = (joi) => ({
+  type: "string",
+  base: joi.string(),
+  messages: {
+    "string.escapeHTML": "{{#label}} must not include HTML!",
+  },
+  rules: {
+    escapeHTML: {
+      validate(value, helpers) {
+        const clean = sanitizeHtml(value, {
+          allowedTags: [],
+          allowedAttributes: {},
+        });
+        if (clean !== value)
+          return helpers.error("string.escapeHTML", { value });
+        return clean;
+      },
+    },
+  },
+});
+
+const Joi = BaseJoi.extend(extension);
 
 export const ProductSchema = Joi.object({
   product: Joi.object({
-    name: Joi.string().required(),
-    shortname: Joi.string().required(),
+    name: Joi.string().required().escapeHTML(),
+    shortname: Joi.string().required().escapeHTML(),
     price: Joi.object({
       current: Joi.number().required().min(0),
       original: Joi.number().required().min(0),
     }).required(),
-    onSale: Joi.boolean().required,
-    inStock: Joi.boolean().required,
-    productType: Joi.string().required(),
-    categories: Joi.array().items(Joi.string().required),
+    onSale: Joi.boolean().required(),
+    inStock: Joi.boolean().required(),
+    productType: Joi.string().required().escapeHTML(),
+    categories: Joi.array().items(Joi.string().required().escapeHTML()),
   }).required(),
 });
