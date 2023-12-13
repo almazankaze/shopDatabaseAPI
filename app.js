2950df6aadbfee4d85dca5f0e954c22fb43d76f4
John Almazan <almazankaze@live.com>
2023-12-12 18:41:41 -0600

    heroku test

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ const sessionConfig = {
     secure: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7,
-    //sameSite: "none",
+    sameSite: "none",
   },
 };
 
